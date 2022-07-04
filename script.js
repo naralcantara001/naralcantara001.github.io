9639653681fe5ee92d728b7b774afdacffa27693
nar <97330396+naralcantara001@users.noreply.github.com>
2022-07-04 21:47:44 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -59,25 +59,7 @@ arrowUp.addEventListener('click', () => {
 })
 
 // skill progress bar
-$(".progress").each(function(){
-  
-    var $bar = $(this).find(".bar");
-    var $val = $(this).find("span");
-    var perc = parseInt( $val.text(), 10);
-  
-    $({p:0}).animate({p:perc}, {
-      duration: 5000,
-      easing: "swing",
-      step: function(p) {
-        $bar.css({
-          transform: "rotate("+ (45+(p*1.8)) +"deg)", // 100%=180° so: ° = % * 1.8
-          // 45 is to add the needed rotation to have the green borders at the bottom
-        });
-        $val.text(p|0);
-      }
-    });
-  });
-  //
+
 
   // scroll animation
   function reveal() {
