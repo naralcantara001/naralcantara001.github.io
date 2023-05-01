5d428dc65789fc0cff0b0fd0136a1cd1559c166b
nar <97330396+naralcantara001@users.noreply.github.com>
2023-05-01 12:39:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -120,3 +120,47 @@ arrowUp.addEventListener('click', () => {
   
 // tab color theme
 // document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#7f1cdf');
+
+// slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-dot", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active-dot";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+let slideIndex1 = 0;
+showSlides1();
+
+function showSlides1() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+  
+  let dots = document.getElementsByClassName("dot1");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex1++;
+  if (slideIndex1 > slides.length) {slideIndex1 = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-dot1", "");
+  }
+  slides[slideIndex1-1].style.display = "block";  
+  dots[slideIndex1-1].className += " active-dot1";
+  setTimeout(showSlides1, 2000); // Change image every 2 seconds
+}
